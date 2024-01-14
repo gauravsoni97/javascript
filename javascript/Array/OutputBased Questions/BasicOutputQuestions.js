0d5649ceb716f65ca04b5168a0574ce2ef2ce6b4
gauravsoni97 <73748616+gauravsoni97@users.noreply.github.com>
2024-01-14 13:24:17 +0530

    basic que

---
+++ javascript/Array/OutputBased Questions/BasicOutputQuestions.js
@@ -0,0 +1,26 @@
+// Basic Array Operations:----------------------------------------------------------------------
+
+// Given an array [1, 2, 3, 4, 5], what would be the result after adding 10 to each element?
+
+// let arr = [1, 2, 3, 4, 5];
+
+// => By using for each we dont get new array
+
+// const updatedArr = arr.forEach((ele) => console.log(ele + 10));
+
+// => By USING MAP = This will return new array
+
+// const updatedArr = arr.map((ele) => ele + 10);
+// console.log(updatedArr);
+
+// ===========================================================================
+
+// Q=> Array Indexing: In the array [10, 20, 30, 40, 50], what is the value at index 2?
+
+// let arr = [10, 20, 30, 40, 50];
+
+// console.log(arr[2]);
+
+
+
+// ===========================================================================
