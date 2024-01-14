c8d565f05458e278a3ffe9142c99de82e5735279
gauravsoni97 <73748616+gauravsoni97@users.noreply.github.com>
2024-01-14 15:08:13 +0530

    50 output based interview questions

--- javascript/Array/OutputBased Questions/BasicOutputQuestions.js
+++ javascript/Array/OutputBased Questions/BasicOutputQuestions.js
@@ -21,6 +21,98 @@
 
 // console.log(arr[2]);
 
+// ===========================================================================
 
+// Array Slicing: If you have an array [1, 2, 3, 4, 5], what would be the result of slicing it to get elements from index 1 to 3?
+
+// let arr = [1, 2, 3, 4, 5];
+
+// console.log(arr.slice(1,3));
 
 // ===========================================================================
+// Array Manipulation:
+
+// Given the array [5, 10, 15, 20, 25], what would be the array after removing the element at index 2?
+
+// let arr = [5, 10, 15, 20, 25];
+
+// const updated = arr.splice(2,1)
+
+// console.log(arr);
+
+// = NOTE => This will manipulate original array
+
+// ===========================================================================
+
+// Multi-dimensional Arrays:
+
+// For a 2D array [[1, 2, 3], [4, 5, 6], [7, 8, 9]], what is the value at the position (2, 3)?
+
+// ===========================================================================
+
+// Conditional Array Operations:
+
+// Create an array containing the squares of numbers from 1 to 5.
+
+// let arr = [];
+
+// for (let i = 0; i < 5; i++) {
+
+//   arr.push(i * i)
+// }
+
+// console.log(arr);
+
+// ===========================================================================
+
+// Array Concatenation:
+
+// Concatenate the arrays [1, 2, 3] and [4, 5, 6] to form a new array.
+
+// let arr1 = [1, 2, 3];
+// let arr2 = [4, 5, 6];
+
+// console.log(arr1.concat(arr2));
+
+// ===========================================================================
+
+// Finding Array Max and Min:
+
+// Determine the maximum and minimum values in the array [12, 5, 23, 8, 17].
+
+// let arr = [12, 5, 23, 8, 17];
+// let min = arr[0];
+// let max = arr[0];
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] < min) {
+//     min = arr[i];
+//   }
+//   if (arr[i] > max) {
+//     max = arr[i];
+//   }
+// }
+// console.log(min, max);
+
+// -----------------------------------------------------------------
+
+// Array Filtering:
+
+// Given the array [2, 4, 6, 8, 10], create a new array containing only the even numbers.
+
+// let arr = [2, 4, 6, 8, 3 ,10];
+
+// const oddNumberArray = arr.filter((ele) => ele % 2);
+// const evenNumberArray = arr.filter((ele) => ele % 2 === 0);
+
+// console.log(oddNumberArray, evenNumberArray);
+
+// --------------------------------------------------------------------
+
+// Using a loop, print each element of the array [3, 7, 11, 15] on a new line.
+
+// let arr = [3, 7, 11, 15];
+
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
--- javascript/js.js
+++
@@ -1,1 +0,0 @@
-console.log("Hello Gaurav, Let's begin");
