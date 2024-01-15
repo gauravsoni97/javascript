f34e3156ca4888e7b19f0a8ab58386e8a2facb5b
gauravsoni97 <73748616+gauravsoni97@users.noreply.github.com>
2024-01-15 19:40:27 +0530

    js interview questions listed

--- InterviewFaced/Interview.js
+++ InterviewFaced/Interview.js
@@ -1,1 +1,84 @@
-console.log("hi");
+// ============ INTERVIEW QUESTIONS BY SINGLE INTERFACE =============== (Rahul Jaggi)
+
+// ----------------------------------------------------------------------
+// Q => Output of this below code ?
+
+// console.log(car);
+// console.log(cars);
+// var car = "BMW";
+// let cars = "Volvo";
+
+// ----------------------------------------------------------------------
+// const arrayData = [{name: 'Rahul', age: '31'}, {name: 'Jaggi', age: '21'}, {name: 'Gaurav', age: '27'}];
+
+// output: [{name: 'Jaggi', age: '21'}, {name: 'Gaurav', age: '27'}, {name: 'Rahul', age: '31'}]
+
+// ----------------------------------------------------------------------
+// Array conversion
+// [1,0,0,1,0,1], output: [1,1,1,0,0,0]
+// ----------------------------------------------------------------------
+// output in console.log()
+// [1,2,3] === [1,2,3]
+// [1,2,3] == [1,2,3]
+// ----------------------------------------------------------------------
+
+// a= [1,2,3];
+// b=a;
+// a === b
+// ----------------------------------------------------------------------
+// null == {}
+// ----------------------------------------------------------------------
+// itterate over this object and return 1,2,3 in li tag  <li><a>1</a></li>
+// var arayObj = {1,2,3};
+// ----------------------------------------------------------------------
+// function newOne(a,b,...c, d){}
+// newOne(1,2,3,5,6,7,8);
+// function newOne(a,b,c,...d,){}
+
+// ----------------------------------------------------------------------
+// let arr = [1, 2, 3, 4, 5, -6, 7];
+// arr.length = 0;
+// console.log(arr);
+// function greeting() {
+//   setTimeout(function () {
+//     console.log(message);
+//   }, 5000);
+//   const message = "Hello, Good morning";
+// }
+// greeting();
+// ----------------------------------------------------------------------
+
+// var expressionOne = function functionOne() {
+//     console.log("functionOne");
+//   };
+//   functionOne();
+// ----------------------------------------------------------------------
+// var currentCity = "NewYork";
+
+// var changeCurrentCity = function () {
+//   console.log("Current City:", currentCity);
+//   var currentCity = "Singapore";
+//   console.log("Current City:", currentCity);
+// };
+
+// changeCurrentCity();
+// ----------------------------------------------------------------------
+
+// setTimeout(() => {
+//     console.log("1");
+//   }, 0);
+//   Promise.resolve("hello").then(() => console.log("2"));
+//   console.log("3");
+// ----------------------------------------------------------------------
+//   const numbers = [11, 25, 31, 23, 33, 18, 200];
+//   numbers.sort();
+//   console.log(numbers);
+// ----------------------------------------------------------------------
+
+// console.log("First line")
+//   [("a", "b", "c")].forEach((element) => console.log(element));
+// console.log("Third line");
+// ----------------------------------------------------------------------
+
+// show that if 1 comes two times then 1 = 2 times and 2 = 1time - like that
+// [1,1,2,3,5,6,4,3,3]
