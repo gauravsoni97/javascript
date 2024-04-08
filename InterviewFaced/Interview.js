258d737a959992b5e77ca444d38152e49c4568a1
gauravsoni97 <gauravsoni8414@gmail.com>
2024-04-08 11:52:46 +0530

    code formattting

--- InterviewFaced/Interview.js
+++ InterviewFaced/Interview.js
@@ -39,6 +39,8 @@
 // let arr = [1, 2, 3, 4, 5, -6, 7];
 // arr.length = 0;
 // console.log(arr);
+
+
 // function greeting() {
 //   setTimeout(function () {
 //     console.log(message);
--- javascript/OutputBasedInterviewQue.js
+++ javascript/OutputBasedInterviewQue.js
@@ -119,21 +119,21 @@
 
 // 9. Write a JavaScript function that returns the Fibonacci sequence up to a given number of terms.
 
-function fibonacciSequence(numTerms) {
-  if (numTerms <= 0) return [];
+// function fibonacciSequence(numTerms) {
+//   if (numTerms <= 0) return [];
 
-  if (numTerms === 1) return [0];
+//   if (numTerms === 1) return [0];
 
-  let sequence = [0, 1];
+//   let sequence = [0, 1];
 
-  while (sequence.length < numTerms) {
-    let nextNumber =
-      sequence[sequence.length - 1] + sequence[sequence.length - 2];
+//   while (sequence.length < numTerms) {
+//     let nextNumber =
+//       sequence[sequence.length - 1] + sequence[sequence.length - 2];
 
-    sequence.push(nextNumber);
-  }
+//     sequence.push(nextNumber);
+//   }
 
-  return sequence;
-}
+//   return sequence;
+// }
 
-console.log(fibonacciSequence(5));
+// console.log(fibonacciSequence(5));
