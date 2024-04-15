6b8e02c88e9c62a6b6142ecedf7ddcc77e4eeab6
gauravsoni97 <gauravsoni8414@gmail.com>
2024-04-15 14:08:45 +0530

    function questions

--- javascript/w3ResourceQueAns/Functions.js
+++ javascript/w3ResourceQueAns/Functions.js
@@ -1,1 +1,15 @@
 console.log("----- Function Que Ans -------");
+// ======================================================
+
+// const num = 1234;
+
+// function NumReverse(number) {
+//   return number.toString().split("").reverse().join("");
+// }
+
+// console.log(NumReverse(num));
+
+// ======================================================
+
+// Write a JavaScript function that checks whether a passed string is a palindrome or not?
+
