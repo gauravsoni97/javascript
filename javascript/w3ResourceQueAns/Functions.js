dbd6368023cc4557a4d9a19d8c515748b1574665
gauravsoni97 <gauravsoni8414@gmail.com>
2024-04-18 13:38:06 +0530

    function k questions

--- javascript/w3ResourceQueAns/Functions.js
+++ javascript/w3ResourceQueAns/Functions.js
@@ -132,8 +132,5 @@ console.log("----- Function Que Ans -------");
 
 // ======================================================
 
-
 // 8. Write a JavaScript function that accepts a number as a parameter and checks whether it is prime or not using recursion.
 // Note : A prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
-
-
