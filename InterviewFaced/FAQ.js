b228098da9da4b606862cc324f5bb037b5e44842
gauravsoni97 <gauravsoni8414@gmail.com>
2024-05-08 11:54:34 +0530

    output based question faq

--- InterviewFaced/FAQ.js
+++ InterviewFaced/FAQ.js
@@ -1,6 +1,7 @@
-// Frequently Asked Javascript Question
+// Frequently Asked Javascript Output Based Question
 
 // 1. Most Frequently occured element in an array
 // 2. Delete Duplicate element from array || Polyfill of new Set()
 // 3. Palindrome number
 // 4. Rotate Array by k times
+// 5. Reverse Array without using revese keyword 
