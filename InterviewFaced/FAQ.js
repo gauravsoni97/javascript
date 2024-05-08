1fba2eeecf5bafc3b2f10f45de9b617badc876c7
gauravsoni97 <gauravsoni8414@gmail.com>
2024-05-08 13:02:03 +0530

    ans added

--- InterviewFaced/FAQ.js
+++ InterviewFaced/FAQ.js
@@ -1,7 +1,62 @@
 // Frequently Asked Javascript Output Based Question
 
 // 1. Most Frequently occured element in an array
+
+// let arr = [1, 2, 3, 4, 4, 5, 3, 4];
+
+// let elem = [];
+// let maxCount = 0;
+
+// let uniq = [...new Set(arr)];
+
+// function reverseArr(arr) {
+//   for (let i = 0; i < uniq.length; i++) {
+//     let count = arr.filter((ele) => ele === uniq[i]).length;
+
+//     if (maxCount < count) {
+//       maxCount = count;
+//       elem = [uniq[i]];
+//     }
+//   }
+//   return elem;
+// }
+
+// console.log(reverseArr(arr));
+
+// --------------------------------------------------------------------
+
 // 2. Delete Duplicate element from array || Polyfill of new Set()
+
+// Answer: let arr = [1, 2, 5, 3, 4, 4, 5, 3, 4];
+// let uniqueArr = arr.filter(
+//   (value, index, self) => self.indexOf(value) === index
+// );
+// console.log(uniqueArr);
+// --------------------------------------------------------------------
+
 // 3. Palindrome number
 // 4. Rotate Array by k times
-// 5. Reverse Array without using revese keyword 
+
+
+// --------------------------------------------------------------------
+
+// 5. Reverse Array without using revese keyword
+// let arr = [1, 24, 3, 4];
+
+// function reverseArr(arr) {
+//   let left = 0;
+//   let right = arr.length - 1;
+
+//   while (left < right) {
+//     let temp = arr[left];
+//     arr[left] = arr[right];
+//     arr[right] = temp;
+
+//     left++;
+//     right--;
+//   }
+//   return arr;
+// }
+
+// console.log(reverseArr(arr));
+
