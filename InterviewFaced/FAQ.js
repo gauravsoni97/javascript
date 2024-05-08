7abab2622010074631e297659dc6cfa5ceb60385
gauravsoni97 <gauravsoni8414@gmail.com>
2024-05-08 11:40:20 +0530

    faq section added

---
+++ InterviewFaced/FAQ.js
@@ -0,0 +1,6 @@
+// Frequently Asked Javascript Question 
+
+// 1. Most Frequently occured element in an array
+// 2. Delete Duplicate element from array || Polyfill of new Set()
+// 3. Palindrome number
+
